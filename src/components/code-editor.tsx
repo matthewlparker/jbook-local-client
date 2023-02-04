e1c6839d76b97b27627fde808c131339f131a175
Matthew <cadburylion@outlook.com>
2023-02-04 00:51:44 -0800

    Added initial value and onChange functionality to editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,22 +1,48 @@
-import MonacoEditor from '@monaco-editor/react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+
+    monacoEditor.getModel().updateOptions({ tabSize: 2 });
+  };
+
+  const onFormatClick = () => {
+    // get current value from editor
+    // format that value
+    // set the formatted value back in the editor
+  };
+
   return (
-    <MonacoEditor
-      language="javascript"
-      theme="dark"
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-      height="500px"
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        value={initialValue}
+        editorDidMount={onEditorDidMount}
+        theme="dark"
+        language="javascript"
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+        height="500px"
+      />
+    </div>
   );
 };
 
