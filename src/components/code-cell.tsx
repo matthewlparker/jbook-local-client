bbcfbf74127675fd199d8f65be9ec0583748671d
Matthew <cadburylion@outlook.com>
2023-02-04 17:39:36 -0800

    Addressed several warnings

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,17 +1,16 @@
 import { useState } from 'react';
 import CodeEditor from '../components/code-editor';
 import Preview from '../components/preview';
-import bundle from '../bundler';
 import Resizable from './resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
+  // const [input, setInput] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  // const onClick = async () => {
+  //   const output = await bundle(input);
+  //   setCode(output);
+  // };
 
   return (
     <Resizable direction="vertical">
@@ -19,7 +18,7 @@ const CodeCell = () => {
         <Resizable direction="horizontal">
           <CodeEditor
             initialValue="const a = 1;"
-            onChange={(value) => setInput(value)}
+            onChange={(value) => setCode(value)}
           />
         </Resizable>
         <Preview code={code} />
