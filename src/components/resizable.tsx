ad9eaec878260331541400dee7fa631aaf095ff2
Matthew <cadburylion@outlook.com>
2023-02-04 17:15:08 -0800

    Handle editor cell relationship with window inner height and width with component state

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import './resizable.css';
+import { useState, useEffect } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
@@ -7,22 +8,36 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerWidth);
   let resizableProps: ResizableBoxProps;
+
+  useEffect(() => {
+    const listener = () => {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+    };
+    window.addEventListener('resize', listener);
+
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
 
   if (direction === 'horizontal') {
     resizableProps = {
       className: 'resize-horizontal',
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [windowWidth * 0.2, Infinity],
+      maxConstraints: [windowWidth * 0.75, Infinity],
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width: windowWidth * 0.75,
       resizeHandles: ['e'],
     };
   } else {
     resizableProps = {
       className: 'resize-vertical',
       minConstraints: [Infinity, 24],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, windowHeight * 0.9],
       height: 300,
       width: Infinity,
       resizeHandles: ['s'],
