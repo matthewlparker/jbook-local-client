71e066162d214ea29353b04ae6305cfd1c3aa54e
Matthew <cadburylion@outlook.com>
2023-02-15 15:22:57 -0800

    Added new AddCell component and render it between every CellListItem and after the last

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,8 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
+import React from 'react';
 
 const CellList: React.FC = () => {
   // Destructure cells from state, and then order & data from cells
@@ -9,10 +12,18 @@ const CellList: React.FC = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
