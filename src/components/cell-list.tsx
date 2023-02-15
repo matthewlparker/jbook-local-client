d3e3ac1600f41a2bedabad3458dbc2fe9603969b
Matthew <cadburylion@outlook.com>
2023-02-15 09:45:58 -0800

    Used new typed selector in CellList

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,10 @@
+import { useTypedSelector } from '../hooks/use-typed-selector';
+
 const CellList: React.FC = () => {
+  // Destructure cells from state, and then order & data from cells
+  // Return array of cells
+  useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
+
   return <div>Cell List</div>;
 };
 
