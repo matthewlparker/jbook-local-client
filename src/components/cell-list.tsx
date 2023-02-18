3cab67794979775c0045dc2723b56899bf5784b1
Matthew <cadburylion@outlook.com>
2023-02-17 16:44:01 -0800

    Application now reads and writes work to file through requests to local-api API

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,9 +1,9 @@
 import './cell-list.css';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
-import React from 'react';
+import { useActions } from '../hooks/use-actions';
 
 const CellList: React.FC = () => {
   // Destructure cells from state, and then order & data from cells
@@ -11,6 +11,12 @@ const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id) => data[id])
   );
+
+  const { fetchCells } = useActions();
+
+  useEffect(() => {
+    fetchCells();
+  }, [fetchCells]);
 
   const renderedCells = cells.map((cell) => (
     <Fragment key={cell.id}>
